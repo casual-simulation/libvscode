f5d57f9a58643077c3f2ccb042d31346d50526a9
Kallyn Gowdy <kallyngowdy@gmail.com>
2021-03-04 13:00:41 -0500

    Add Github1s

---
+++ scripts/hash.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+cd "$(dirname "${0}")/.."
+APP_ROOT=$(pwd)
+
+COMMIT_ID=$(git rev-parse HEAD)
+SHORT_COMMIT_ID=${COMMIT_ID:0:7}
+STATIC_FOLDER_NAME="static-${SHORT_COMMIT_ID-unknown}"
+
+if [ -e "${APP_ROOT}/dist/static" ]; then
+	mv "${APP_ROOT}/dist/static" "${APP_ROOT}/dist/${STATIC_FOLDER_NAME}"
+	sed "s/{STATIC_FOLDER}/${STATIC_FOLDER_NAME}/" "${APP_ROOT}/resources/index-hash.html" > "${APP_ROOT}/dist/404.html"
+	rm -f "${APP_ROOT}/dist/index.html" # remove the exists `index.html` file
+	echo "hash static files done"
+else
+	echo "Please build github1s first"
+fi
